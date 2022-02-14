41dc26c4b0e707a898dc18d231556e6056adb176
xopenwrt <98641952+xopenwrt@users.noreply.github.com>
2022-02-14 23:58:21 +0800

    fix bug

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 BUILD_LOCAL() {
-  cd alist-web
   yarn
   yarn build
   mv dist/index.html dist/local.html
   mv dist/* ../dist
-  cd ..
 }
 
 BUILD_JSDELIVR() {
-  cd alist-web
   yarn
   webCommit=$(git log --pretty=format:"%h" -1)
   gitTag=$(git describe --long --tags --always)
@@ -24,25 +21,20 @@ BUILD_JSDELIVR() {
   git config --local user.name "xinb"
   git commit --allow-empty -m "upload $gitTag assets files" -a
   git tag -a $gitTag -m "upload $gitTag assets files"
-  cd ..
 }
 
 BUILD_UNPKG_ZHIMG() {
-  cd alist-web
   version=$(git describe --abbrev=0 --tags)
   sed -i -e "s/0.0.0/$version/g" package.json
   yarn build --base="https://unpkg.zhimg.com/alist-web@$version/dist"
   cp dist/index.html ../dist/zhimg.html
-  cd ..
 }
 
 BUILD_UNPKG() {
-  cd alist-web
   version=$(git describe --abbrev=0 --tags)
   sed -i -e "s/0.0.0/$version/g" package.json
   yarn build --base="https://unpkg.com/alist-web@$version/dist"
   cp dist/index.html ../dist/unpkg.html
-  cd ..
 }
 
 MAKE_RELEASE() {
